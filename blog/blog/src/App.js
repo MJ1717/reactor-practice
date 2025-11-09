3ae59a737cb32c328bb4339a8d29978d7bb933dd
MJ1717 <bananahamo77@gmail.com>
2025-11-09 17:21:27 -0500

    added react

---
+++ blog/blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="black-nav">
+        <h4>Blog</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
